a99a4e096820ac4e6196674e53a49010b50c4c21
sierra392 <ssm24816@gmail.com>
2025-03-10 12:39:53 -0700

    changes

---
+++ youlostit-ble/Debug/Core/Src/lsm6dsl.d
@@ -0,0 +1,18 @@
+Core/Src/lsm6dsl.o: ../Core/Src/lsm6dsl.c ../Core/Inc/lsm6dsl.h \
+ ../Core/Inc/i2c.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l475xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+../Core/Inc/lsm6dsl.h:
+../Core/Inc/i2c.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l475xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
